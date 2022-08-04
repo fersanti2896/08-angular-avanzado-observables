dac2f5a55e8bdd3b0affbb406bf842a5f4d67ee7
Fernando Nicolás <fersanti2896@gmail.com>
2022-08-04 01:55:39 -0500

    Subiendo proyecto de S7

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router'
+
+import { FormsModule } from '@angular/forms';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+    AccountSettingsComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule,
+    FormsModule, 
+    SharedModule,
+    RouterModule
+  ], 
+  exports: [ 
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent, 
+    AccountSettingsComponent
+  ]
+})
+export class PagesModule { }
---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+  menu: any[] = [
+    {
+      titulo: 'Dashboard',
+      icon: 'mdi mdi-gauge',
+      submenu: [
+        { titulo: 'Main', url: '/' },
+        { titulo: 'Progress Bar', url: '/dashboard/progress' },
+        { titulo: 'Gráficas', url: '/dashboard/grafica1' }
+      ]
+    }
+  ];
+
+  constructor() { }
+}
